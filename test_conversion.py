1dad0ee04e98db87b8db867930b1f33316d6af4d
arkady <arkady>
2021-03-03 11:38:23 +0200

    quotes

--- test_conversion.py
+++ test_conversion.py
@@ -1,6 +1,11 @@
 import pytest
 
+quotes = {}
+def quote_listener(base, target, rate):
+    quotes[f"base:target"] = rate
 
 def test_quotes():
     pairs = [("USD","ILS"), ("USD", "GBP")]
-    quotes = Quotes(pairs, 6.0, )
+    quotes = Quotes(pairs, 6.0, [quote_listener])
+    time.sleep(6.1)
+    for base, target in pairs:
