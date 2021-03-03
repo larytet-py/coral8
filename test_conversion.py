66b3a99af43e0eb8cfad4591d483694fb2cf0ad3
arkady <arkady>
2021-03-03 11:43:03 +0200

    quotes

--- test_conversion.py
+++ test_conversion.py
@@ -1,11 +1,28 @@
 import pytest
 
 quotes = {}
-def quote_listener(base, target, rate):
-    quotes[f"base:target"] = rate
+def get_key(base, target):
+    return f"base:target"
 
-def test_quotes():
+def quote_listener(base, target, rate):
+    quotes[get_key(base, target)] = rate
+
+def test_quotes_async():
     pairs = [("USD","ILS"), ("USD", "GBP")]
     quotes = Quotes(pairs, 6.0, [quote_listener])
     time.sleep(6.1)
     for base, target in pairs:
+        key = get_key(base, target)
+        assert key in quotes, f"Key {key} is missing in quotes"
+    quotes.close()
+
+def test_quotes_sync():
+    pairs = [("USD","ILS")]
+    quotes = Quotes(pairs, 6.0, [quote_listener])
+    time.sleep(1.0)
+    rate, err = quotes.quote("USD", "ILS")
+    assert err == None, f"Error is not None {err}"    
+    rate, err = quotes.quote("GBP", "ILS")
+    assert err != None, f"Error is None {err}"
+    quotes.close()
+
